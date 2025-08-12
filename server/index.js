f59ec9f3ea175caa0753cfc364ae3e475401a593
milan patanvadiya <milanpatanvadiya18@gmail.com>
2025-08-12 10:07:31 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 
-const app = express(); 
+const app = express();
 
-const dotenv = require('dotenv');  
+const dotenv = require('dotenv');
 dotenv.config();
 
-const connectDB = require('./config/database');  
+const connectDB = require('./config/database');
 connectDB()
 
 //cloudinary setup start
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
@@ -25,10 +25,10 @@ const PORT = process.env.PORT || 8000
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.use('/',require('./routes/indexRoute'));
+app.use('/', require('./routes/indexRoute'));
 
-app.listen(PORT,(err)=>{
-    if(err){
+app.listen(PORT, (err) => {
+    if (err) {
         console.log(err);
         return false;
     }
